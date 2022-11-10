2f458747f221b28ada0ad455e6ab9b9b47594d1a
Fabricio <fabriciospfullstack@gmail.com>
2022-11-10 16:57:36 -0300

    :sparkles: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
+import { Analytics } from '@vercel/analytics/react'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
